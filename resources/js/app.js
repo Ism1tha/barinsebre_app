e4aff469065723bd7ce1a7185c88e6383da9fbe1
Ism1tha <xgalan@hotmail.com>
2023-09-04 18:27:42 +0200

    PrimeVue & PrimeIcons installation

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,11 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 
+import PrimeVue from 'primevue/config';
+import "primevue/resources/themes/tailwind-light/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
+
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
@@ -15,6 +20,7 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .use(PrimeVue)
             .mount(el);
     },
     progress: {
